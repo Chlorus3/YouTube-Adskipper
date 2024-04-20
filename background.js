de5ee1cbcbbe4d3e773850e343bf97da0e167772
Chlorus <99441357+Chlorus3@users.noreply.github.com>
2024-04-20 11:42:43 +0100

    Script will only inject if 'watch' is in URL

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (tab.url && tab.url.includes('youtube.com') && changeInfo.status === 'complete') {
+    if (tab.url && tab.url.includes('youtube.com/watch') && changeInfo.status === 'complete') {
         chrome.scripting.executeScript({
             target: { tabId: tabId },
             files: ['inject.js']
